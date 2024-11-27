a0acf0b4ecc40beb8e07b59b36d09a0ebf1ad67b
Skoushik54 <140392717+Skoushik54@users.noreply.github.com>
2024-11-27 21:49:04 +0530

    inital commit

---
+++ file1.py
@@ -0,0 +1,19 @@
+print("file 1 in master")
+# git init
+# git add .
+# git commit -m "Initial commit added all the files"
+# git push -u origin master
+# git branch branch1
+# git switch branch1
+# git add .
+# git commit -m "modified in branch1"
+# git switch master
+# git branch branch2
+# git switch branch2
+# git add .
+# git commit -m "modified in branch2"
+# git switch master
+# git merge branch1
+# git merge branch2
+# git add .
+# git commit -m "resolved comflict"
---
+++ file3.java
@@ -0,0 +1,5 @@
+class file3{
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+    }
+}
